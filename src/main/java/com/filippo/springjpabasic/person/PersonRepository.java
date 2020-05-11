1bef6b48e5de10a498bab2108cbd53fa688541a1
filippocazzarolli <f.cazzarolli@ifinet.it>
2020-05-11 07:19:21 +0200

    wip

--- src/main/java/com/filippo/springjpabasic/person/PersonRepository.java
+++ src/main/java/com/filippo/springjpabasic/person/PersonRepository.java
@@ -1,7 +1,9 @@
 package com.filippo.springjpabasic.person;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PersonRepository extends JpaRepository<PersonEntity, Long> {
     PersonEntity findById(long id);
 }
